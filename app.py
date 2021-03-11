5163d55766fb743c240239c270b4c84da5615f0b
john00neill <john2984@live.com>
2021-03-11 23:35:29 +0000

    Register function in app.py checks if username exists and displays appropriate message and redirects

--- app.py
+++ app.py
@@ -28,7 +28,23 @@ def get_chants():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     return render_template("register.html")
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
 
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect((url_for("profile", username=session["user"])))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
