5d385a6c77754ae74dbab7d318a2c94b2e3658ae
john00neill <john2984@live.com>
2021-03-27 15:02:12 +0000

    Add_chants file added and chants file updated on accordion display

--- app.py
+++ app.py
@@ -137,6 +137,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_chant")
+def add_chant():
+    return render_template("add_chant.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
